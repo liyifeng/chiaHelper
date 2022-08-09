74bc495b5e33db6b6fec52e470f45eacbbfc0e45
liyf <liyf@bbtree.com>
2022-08-09 13:34:43 +0800

    完善启动脚本

--- release/start.bat
+++ release/start.bat
@@ -1,7 +1,28 @@
 @echo off
+title=Chia auto transfer
 set curDir=%cd%
 set JAVA_HOME=%curDir%\jdk8u332-b09-jre
 set PATH=%JAVA_HOME%\bin
 set CLASSPATH=.;%JAVA_HOME%\lib;%JAVA_HOME%\lib\tools.jar
 
+:head
+set option=""
+echo --------------------------------------------------------------------------------------------
+echo 1.启动程序
+echo 2.启动且保留黑窗口（软件运行不正常时候便于观察运行日志）
+echo --------------------------------------------------------------------------------------------
+set /p option=请输入你要执行的序号:
+echo loading...
+
+if "%option%"=="0" goto exit
+if "%option%"=="" goto start
+if "%option%"=="1" goto start
+if "%option%"=="2" goto startAndKeepTerminal
+
+:start
 start javaw -jar chiaAutoTransfer.jar
+exit
+
+:startAndKeepTerminal
+java -jar chiaAutoTransfer.jar
+goto head
